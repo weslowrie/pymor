2e4d9e8a49e12eb62b4c165e38774c75c290c91e
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2019-07-11 20:10:54 +0200

    [algorithms.svd_va] add "method of snapshots" method

--- src/pymor/algorithms/svd_va.py
+++ src/pymor/algorithms/svd_va.py
@@ -2,12 +2,119 @@
 # Copyright 2013-2019 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
+"""Module for SVD method of operators represented by |VectorArrays|."""
+
+import numpy as np
 import scipy.linalg as spla
 
 from pymor.algorithms.gram_schmidt import gram_schmidt
+from pymor.core.exceptions import AccuracyError
 from pymor.core.logger import getLogger
 from pymor.operators.interfaces import OperatorInterface
+from pymor.tools.floatcmp import float_cmp_all
 from pymor.vectorarrays.interfaces import VectorArrayInterface
+
+
+def mos(A, modes=None, product=None, rtol=4e-8, atol=0., l2_err=0., orthonormalize=True,
+        check=True, check_tol=1e-10):
+    """Method of snapshots.
+
+    Viewing the |VectorArray| `A` as a `A.dim` x `len(A)` matrix,
+    the return value of this method is the |VectorArray| of left-singular
+    vectors of the singular value decomposition of `A`, where the inner product
+    on R^(`dim(A)`) is given by `product` and the inner product on R^(`len(A)`)
+    is the Euclidean inner product.
+
+    Parameters
+    ----------
+    A
+        The |VectorArray| for which the POD is to be computed.
+    modes
+        If not `None`, only the first `modes` POD modes (singular vectors) are
+        returned.
+    product
+        Inner product |Operator| w.r.t. which the POD is computed.
+    rtol
+        Singular values smaller than this value multiplied by the largest singular
+        value are ignored.
+    atol
+        Singular values smaller than this value are ignored.
+    l2_err
+        Do not return more modes than needed to bound the l2-approximation
+        error by this value. I.e. the number of returned modes is at most ::
+
+            argmin_N { sum_{n=N+1}^{infty} s_n^2 <= l2_err^2 }
+
+        where `s_n` denotes the n-th singular value.
+    orthonormalize
+        If `True`, orthonormalize the computed POD modes again using
+        the :func:`~pymor.algorithms.gram_schmidt.gram_schmidt` algorithm.
+    check
+        If `True`, check the computed POD modes for orthonormality.
+    check_tol
+        Tolerance for the orthonormality check.
+
+    Returns
+    -------
+    POD
+        |VectorArray| of POD modes.
+    SVALS
+        Sequence of singular values.
+    """
+
+    assert isinstance(A, VectorArrayInterface)
+    assert len(A) > 0
+    assert modes is None or modes <= len(A)
+    assert product is None or isinstance(product, OperatorInterface)
+
+    logger = getLogger('pymor.algorithms.pod.pod')
+
+    with logger.block(f'Computing Gramian ({len(A)} vectors) ...'):
+        B = A.gramian(product)
+
+    with logger.block('Computing eigenvalue decomposition ...'):
+        eigvals = (None
+                   if modes is None or l2_err > 0.
+                   else (len(B) - modes, len(B) - 1))
+
+        evals, Vh = spla.eigh(B, overwrite_a=True, turbo=True, eigvals=eigvals)
+        evals = evals[::-1]
+        Vh = Vh.T[::-1, :]
+
+        tol = max(rtol ** 2 * evals[0], atol ** 2)
+        above_tol = np.where(evals >= tol)[0]
+        if len(above_tol) == 0:
+            return A.space.empty(), np.array([]), np.zeros((0, len(A)))
+        last_above_tol = above_tol[-1]
+
+        errs = np.concatenate((np.cumsum(evals[::-1])[::-1], [0.]))
+        below_err = np.where(errs <= l2_err**2)[0]
+        first_below_err = below_err[0]
+
+        selected_modes = min(first_below_err, last_above_tol + 1)
+        if modes is not None:
+            selected_modes = min(selected_modes, modes)
+
+        s = np.sqrt(evals[:selected_modes])
+        Vh = Vh[:selected_modes]
+
+    with logger.block(f'Computing left-singular vectors ({len(Vh)} vectors) ...'):
+        U = A.lincomb(Vh / s[:, np.newaxis])
+
+    if orthonormalize:
+        with logger.block('Re-orthonormalizing U modes ...'):
+            U = gram_schmidt(U, product=product, copy=False)
+
+    if check:
+        logger.info('Checking orthonormality ...')
+        if not float_cmp_all(U.inner(U, product), np.eye(len(U)), atol=check_tol,
+                             rtol=0.):
+            err = np.max(np.abs(U.inner(U, product) - np.eye(len(U))))
+            raise AccuracyError(f'result not orthogonal (max err={err})')
+        if len(U) < len(Vh):
+            raise AccuracyError('additional orthonormalization removed basis vectors')
+
+    return U, s, Vh
 
 
 def qr_svd(A, product=None):
